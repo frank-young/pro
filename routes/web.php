34a4582862df40f683a59788d5bfbd588c722f66
frank <yangjunalns@qq.com>
2017-03-06 15:52:06 +0800

    link and something

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::get('/', function () {
   get('/about', 'StaticPagesController@about');
  */
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
