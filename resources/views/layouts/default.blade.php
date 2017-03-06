bbd1ae576bb8f4c7473bf004b92d5483b26297c5
frank <yangjunalns@qq.com>
2017-03-06 16:54:02 +0800

    filling layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,24 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title', 'Sample') - Laravel 入门教程</title>
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
-  <header class="navbar navbar-fixed-top navbar-inverse">
-      <div class="container">
-        <div class="col-md-offset-1 col-md-10">
-          <a href="/" id="logo">Sample App</a>
-          <nav>
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="/help">帮助</a></li>
-              <li><a href="#">登录</a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </header>
+    @include('layouts._header')
     <div class="container">
       @yield('content')
+       @include('layouts._footer')
     </div>
 </body>
 </html>
